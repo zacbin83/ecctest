c0544ce4b88f1c761bcc2fc0b6ab777019e03091
zacbin83 <zacbinloL@gmail.com>
2022-11-18 21:18:10 +0700

    e

--- client/src/pages/Admin/Admin.js
+++ client/src/pages/Admin/Admin.js
@@ -13,9 +13,9 @@ const initialFormData = {
 };
 
 
-const Home = () => {
+const Home = () => {    
     const [formData, setFormData] = useState(initialFormData);
-    
+    document.getElementById('datePicker').valueAsDate = new Date();
     const handleOnChange = (e) => {
         const { name, value } = e.target;
         console.log(formData)
@@ -60,10 +60,10 @@ const Home = () => {
                 <symbol id="down" viewBox="0 0 16 16">
                     <polygon points="3.81 4.38 8 8.57 12.19 4.38 13.71 5.91 8 11.62 2.29 5.91 3.81 4.38" />
                 </symbol>
-                <symbol id="users" viewBox="0 0 16 16">
+                <symbol id="membe1rs" viewBox="0 0 16 16">
                     <path d="M8,0a8,8,0,1,0,8,8A8,8,0,0,0,8,0ZM8,15a7,7,0,0,1-5.19-2.32,2.71,2.71,0,0,1,1.7-1,13.11,13.11,0,0,0,1.29-.28,2.32,2.32,0,0,0,.94-.34,1.17,1.17,0,0,0-.27-.7h0A3.61,3.61,0,0,1,5.15,7.49,3.18,3.18,0,0,1,8,4.07a3.18,3.18,0,0,1,2.86,3.42,3.6,3.6,0,0,1-1.32,2.88h0a1.13,1.13,0,0,0-.27.69,2.68,2.68,0,0,0,.93.31,10.81,10.81,0,0,0,1.28.23,2.63,2.63,0,0,1,1.78,1A7,7,0,0,1,8,15Z" />
                 </symbol>
-                <symbol id="collection" viewBox="0 0 16 16">
+                <symbol id="member5s" viewBox="0 0 16 16">
                     <rect width="7" height="7" />
                     <rect y="9" width="7" height="7" />
                     <rect x="9" width="7" height="7" />
@@ -76,13 +76,13 @@ const Home = () => {
                     <rect x="8" y="6" width="2" height="10" />
                     <rect x="4" y="11" width="2" height="5" />
                 </symbol>
-                <symbol id="comments" viewBox="0 0 16 16">
+                <symbol id="blogs" viewBox="0 0 16 16">
                     <path d="M0,16.13V2H15V13H5.24ZM1,3V14.37L5,12h9V3Z" />
                     <rect x="3" y="5" width="9" height="1" />
                     <rect x="3" y="7" width="7" height="1" />
                     <rect x="3" y="9" width="5" height="1" />
                 </symbol>
-                <symbol id="pages" viewBox="0 0 16 16">
+                <symbol id="members" viewBox="0 0 16 16">
                     <rect x="4" width="12" height="12" transform="translate(20 12) rotate(-180)" />
                     <polygon points="2 14 2 2 0 2 0 14 0 16 2 16 14 16 14 14 2 14" />
                 </symbol>
@@ -102,7 +102,7 @@ const Home = () => {
                     <rect x="12.84" y="11.03" width="1" height="4.97" />
                     <polygon points="11.85 11.47 13.34 8.88 14.84 11.47 11.85 11.47" />
                 </symbol>
-                <symbol id="options" viewBox="0 0 16 16">
+                <symbol id="s" viewBox="0 0 16 16">
                     <path d="M8,11a3,3,0,1,1,3-3A3,3,0,0,1,8,11ZM8,6a2,2,0,1,0,2,2A2,2,0,0,0,8,6Z" />
                     <path d="M8.5,16h-1A1.5,1.5,0,0,1,6,14.5v-.85a5.91,5.91,0,0,1-.58-.24l-.6.6A1.54,1.54,0,0,1,2.7,14L2,13.3a1.5,1.5,0,0,1,0-2.12l.6-.6A5.91,5.91,0,0,1,2.35,10H1.5A1.5,1.5,0,0,1,0,8.5v-1A1.5,1.5,0,0,1,1.5,6h.85a5.91,5.91,0,0,1,.24-.58L2,4.82A1.5,1.5,0,0,1,2,2.7L2.7,2A1.54,1.54,0,0,1,4.82,2l.6.6A5.91,5.91,0,0,1,6,2.35V1.5A1.5,1.5,0,0,1,7.5,0h1A1.5,1.5,0,0,1,10,1.5v.85a5.91,5.91,0,0,1,.58.24l.6-.6A1.54,1.54,0,0,1,13.3,2L14,2.7a1.5,1.5,0,0,1,0,2.12l-.6.6a5.91,5.91,0,0,1,.24.58h.85A1.5,1.5,0,0,1,16,7.5v1A1.5,1.5,0,0,1,14.5,10h-.85a5.91,5.91,0,0,1-.24.58l.6.6a1.5,1.5,0,0,1,0,2.12L13.3,14a1.54,1.54,0,0,1-2.12,0l-.6-.6a5.91,5.91,0,0,1-.58.24v.85A1.5,1.5,0,0,1,8.5,16ZM5.23,12.18l.33.18a4.94,4.94,0,0,0,1.07.44l.36.1V14.5a.5.5,0,0,0,.5.5h1a.5.5,0,0,0,.5-.5V12.91l.36-.1a4.94,4.94,0,0,0,1.07-.44l.33-.18,1.12,1.12a.51.51,0,0,0,.71,0l.71-.71a.5.5,0,0,0,0-.71l-1.12-1.12.18-.33a4.94,4.94,0,0,0,.44-1.07l.1-.36H14.5a.5.5,0,0,0,.5-.5v-1a.5.5,0,0,0-.5-.5H12.91l-.1-.36a4.94,4.94,0,0,0-.44-1.07l-.18-.33L13.3,4.11a.5.5,0,0,0,0-.71L12.6,2.7a.51.51,0,0,0-.71,0L10.77,3.82l-.33-.18a4.94,4.94,0,0,0-1.07-.44L9,3.09V1.5A.5.5,0,0,0,8.5,1h-1a.5.5,0,0,0-.5.5V3.09l-.36.1a4.94,4.94,0,0,0-1.07.44l-.33.18L4.11,2.7a.51.51,0,0,0-.71,0L2.7,3.4a.5.5,0,0,0,0,.71L3.82,5.23l-.18.33a4.94,4.94,0,0,0-.44,1.07L3.09,7H1.5a.5.5,0,0,0-.5.5v1a.5.5,0,0,0,.5.5H3.09l.1.36a4.94,4.94,0,0,0,.44,1.07l.18.33L2.7,11.89a.5.5,0,0,0,0,.71l.71.71a.51.51,0,0,0,.71,0Z" />
                 </symbol>
@@ -131,134 +131,34 @@ const Home = () => {
                         <li className="menu-heading">
                             <h3>Admin</h3>
                         </li>
+                        
                         <li>
                             <a href="#0">
                                 <svg>
-                                    <use href="#pages"></use>
+                                    <use href="#blogs"></use>
                                 </svg>
-                                <span>Pages</span>
+                                <span>Blogs</span>
                             </a>
                         </li>
                         <li>
                             <a href="#0">
                                 <svg>
-                                    <use href="#users"></use>
+                                    <use href="#members"></use>
                                 </svg>
-                                <span>Users</span>
+                                <span>Member</span>
                             </a>
                         </li>
-                        <li>
-                            <a href="#0">
-                                <svg>
-                                    <use href="#options"></use>
-                                </svg>
-                                <span>Edit</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#0">
-                                <svg>
-                                    <use href="#trends"></use>
-                                </svg>
-                                <span>Trends</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#0">
-                                <svg>
-                                    <use href="#collection"></use>
-                                </svg>
-                                <span>Collection</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#0">
-                                <svg>
-                                    <use href="#comments"></use>
-                                </svg>
-                                <span>Bình luận</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#0">
-                                <svg>
-                                    <use href="#appearance"></use>
-                                </svg>
-                                <span>Appearance</span>
-                            </a>
-                        </li>
-                        <li className="menu-heading">
-                            <h3>Settings</h3>
-                        </li>
-                        <li>
-                            <a href="#0">
-                                <svg>
-                                    <use href="#settings"></use>
-                                </svg>
-                                <span>Settings</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#0">
-                                <svg>
-                                    <use href="#options"></use>
-                                </svg>
-                                <span>Options</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#0">
-                                <svg>
-                                    <use href="#charts"></use>
-                                </svg>
-                                <span>Charts</span>
-                            </a>
-                        </li>
-                        <li>
-                            <div className="switch">
-                                <input type="checkbox" id="mode" checked />
-                                <label for="mode">
-                                    <span></span>
-                                    <span>Dark</span>
-                                </label>
-                            </div>
-                            <button className="collapse-btn" aria-expanded="true" aria-label="collapse menu">
-                                <svg aria-hidden="true">
-                                    <use href="#collapse"></use>
-                                </svg>
-                                <span>Collapse</span>
-                            </button>
-                        </li>
+                        
+                        
                     </ul>
                 </nav>
             </header>
             <section className="page-content">
-                <section className="search-and-user">
-                    <form>
-                        <input
-                            type="search"
-                            placeholder="Search Pages..."
-                        />
-                        <button type="submit" aria-label="submit form">
-                            <svg aria-hidden="true">
-                                <use href="#search"></use>
-                            </svg>
-                        </button>
-                    </form>
-                    <div className="admin-profile">
-                        <span className="greeting">Hello admin</span>
-                        <div className="notifications">
-                            <span className="badge">1</span>
-                            <svg>
-                                <use href="#users"></use>
-                            </svg>
-                        </div>
-                    </div>
-                </section>
+                
                 
                 <form onSubmit={handleOnSubmit}>
                 <section >
-                    <p className="tittle-name input">POST TITTLE </p>               
+                    <p className="tittle-name input">Tiêu đề </p>               
                          <input 
                             className="input-tittle"
                             type="text"
@@ -269,18 +169,19 @@ const Home = () => {
                             placeholder="Nhập tiêu đề..."
                             required
                          ></input>
-                    <p className="category-name input">CATEGORY </p>               
+                    <p className="category-name input">Ngày tạo</p>               
                          <input 
                             className="input-tittle"
-                            type="text"
+                            type="date"
                             name="category"
                             value={formData.category}
                             onChange={handleOnChange}
                             class="form-control"
                             placeholder="Nhập loại..."
+                            id="datePicker"
                             required
                         ></input>
-                    <p className="img-name input">IMAGE </p>               
+                    <p className="img-name input">Nội dung </p>               
                         <FileBase
                             required
                             type="file"
@@ -297,7 +198,7 @@ const Home = () => {
                                 content: value,
                               });}}
                             className="form-control"
-                            style={{ width: '100%', height: '100%' }} modules={{
+                            style={{ width: '100%', height: '500px' }} modules={{
                             toolbar: [
                                 [{ 'header': '1' }, { 'header': '2' }, { 'font': [] }],
                                 [{ size: [] }],
